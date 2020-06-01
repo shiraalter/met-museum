0ae0abad9100a0a8f9d804990b04a7a49d84355c
shiraalter <shiraalter@gmail.com>
2020-06-01 13:42:54 -0400

    fixed code format

--- src/main/java/alter/museum/MetService.java
+++ src/main/java/alter/museum/MetService.java
@@ -9,17 +9,16 @@ import retrofit2.http.Query;
 public interface MetService {
 
     // department list
-     @GET("/public/collection/v1/departments")
-     Call<MetFeed.DepartmentObjects> getDepartmentList();
+    @GET("/public/collection/v1/departments")
+    Call<MetFeed.DepartmentObjects> getDepartmentList();
 
     // list of object IDs
     @GET("/public/collection/v1/objects")
     Call<MetFeed.ObjectList> getObjectID(@Query("departmentIds") int departmentID);
 
-     //specific object info
-     @GET("/public/collection/v1/objects/{objectID}")
-     Call<MetFeed.ObjectInfo> getObjectInfo(@Path("objectID") int objectID);
-
+    //specific object info
+    @GET("/public/collection/v1/objects/{objectID}")
+    Call<MetFeed.ObjectInfo> getObjectInfo(@Path("objectID") int objectID);
 
 
 }
