530b73761df11f946bc673a1c26b4f200d522915
shiraalter <shiraalter@gmail.com>
2020-06-02 12:51:37 -0400

    added mockito

---
+++ src/test/java/alter/museum/MetControllerTest.java
@@ -0,0 +1,20 @@
+package alter.museum;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MetControllerTest {
+
+    //test for department list
+    @Test
+    public void onResponseDepartments(){
+        //given
+        MetService service = mock(MetService.class);
+
+        //when
+
+        //then
+    }
+
+}
