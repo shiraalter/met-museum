f9b12dcdf8163087f1cbd5a427d284976b5b3a49
shiraalter <shiraalter@gmail.com>
2020-06-02 21:21:17 -0400

    mockito test

--- src/test/java/alter/museum/MetControllerTest.java
+++ src/test/java/alter/museum/MetControllerTest.java
@@ -1,20 +1,66 @@
 package alter.museum;
 
 import org.junit.Test;
+import retrofit2.Call;
+import retrofit2.Response;
+
+
+import javax.swing.*;
+
+import java.awt.*;
+import java.util.List;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 public class MetControllerTest {
 
-    //test for department list
+    //test request for department list
     @Test
-    public void onResponseDepartments(){
+    public void requestDepartmentList() {
         //given
         MetService service = mock(MetService.class);
+        Call<MetFeed> call = mock(Call.class);
+        JLabel label = mock(JLabel.class);
+        JComboBox<MetFeed.DepartmentObjects.Departments> comboBox = mock(JComboBox.class);
+        MetFrame frame = mock(MetFrame.class);
+
+        doReturn(call).when(service).getDepartmentList();
+
+        MetController controller = new MetController(service, frame, label, label, label, label, label, comboBox);
 
         //when
+        controller.requestDepartmentList();
 
         //then
+        verify(call).enqueue(any());
+    }
+
+    @Test
+    public void populateComboBox() {
+
+        //given
+        MetService service = mock(MetService.class);
+        JLabel label = mock(JLabel.class);
+        JComboBox<MetFeed.DepartmentObjects.Departments> comboBox = mock(JComboBox.class);
+        MetFrame frame = mock(MetFrame.class);
+
+        MetController controller = new MetController(service, frame, label, label, label, label, label, comboBox);
+        List<MetFeed.DepartmentObjects.Departments> departmentList = mock(List.class);
+
+        Response<MetFeed> response = mock(Response.class);  //needed?
+
+        //what object is calling the populateCombobox aside from response?
+        doReturn(departmentList).when(response).body();
+        doReturn(comboBox).when(response).body();
+
+        //when
+        controller.populateComboBox(departmentList, comboBox);
+
+        //then
+       // verify(comboBox   );
+
+
     }
 
 }
