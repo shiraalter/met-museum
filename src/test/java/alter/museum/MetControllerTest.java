625be4e4a57464e7a9d0d0b38843f4bd387830e1
shiraalter <shiraalter@gmail.com>
2020-06-05 00:02:21 -0400

    added new tests

--- src/test/java/alter/museum/MetControllerTest.java
+++ src/test/java/alter/museum/MetControllerTest.java
@@ -7,6 +7,7 @@ import retrofit2.Response;
 
 import javax.swing.*;
 
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 import static org.mockito.Mockito.*;
@@ -27,6 +28,8 @@ public class MetControllerTest {
 
         MetController controller = new MetController(service, frame, label, label, label, label, label, comboBox);
 
+
+
         //when
         controller.requestDepartmentList();
 
@@ -42,7 +45,6 @@ public class MetControllerTest {
         JLabel label = mock(JLabel.class);
         JComboBox<MetFeed.DepartmentObjects.Departments> comboBox = mock(JComboBox.class);
         MetFrame frame = mock(MetFrame.class);
-        Response<MetFeed> response = mock(Response.class);  //needed?
 
         MetController controller = new MetController(service, frame, label, label, label, label, label, comboBox);
 
@@ -55,9 +57,12 @@ public class MetControllerTest {
         list.departments = departmentList;
 
 
+        //test passes without following code:
+   /*     //Response<MetFeed> response = mock(Response.class);
+
         //test still passes without the doReturns + response
         doReturn(list).when(response).body();
-        doReturn(comboBox).when(response).body();
+        doReturn(comboBox).when(response).body();*/
 
         //when
         controller.populateComboBox(departmentList, comboBox);
@@ -67,5 +72,83 @@ public class MetControllerTest {
 
 
     }
+
+    @Test
+    public void requestOBjectList() {
+
+        //given
+        MetService service = mock(MetService.class);
+        Call<MetFeed> call = mock(Call.class);
+        JLabel label = mock(JLabel.class);
+        JComboBox<MetFeed.DepartmentObjects.Departments> comboBox = mock(JComboBox.class);
+        MetFrame frame = mock(MetFrame.class);
+        int id = 5;
+
+        doReturn(call).when(service).getObjectID(id);
+
+        MetController controller = new MetController(service, frame, label, label, label, label, label, comboBox);
+
+        //when
+        controller.requestObjectList(id);
+
+        //then
+        verify(call).enqueue(any());
+
+    }
+
+    @Test
+    public void createAndSendObjectList(){
+        //given
+        Response<MetFeed> response = mock(Response.class);
+        MetService service = mock(MetService.class);
+        JLabel label = mock(JLabel.class);
+        JComboBox<MetFeed.DepartmentObjects.Departments> comboBox = mock(JComboBox.class);
+        MetFrame frame = mock(MetFrame.class);
+        ArrayList<Integer> mockIDList = mock(ArrayList.class);    //mock ArrayList to pass to method to populate
+        MetController controller = new MetController(service, frame, label, label, label, label, label, comboBox);
+
+        //create MetFeed object and add object ArrayList values
+        int id = 2;
+        MetFeed.ObjectList list = new MetFeed.ObjectList();
+        ArrayList<Integer> idList = new ArrayList<>();
+        idList.add(id);
+        list.objectIDs = idList;
+
+        doReturn(frame).when(response).body();
+
+        //when
+        controller.createAndSendObjectList(list);
+
+        //then
+        verify(mockIDList).equals(list.objectIDs);      //verify that the method populates objectIDArray with the listOfIds.objectIDs
+        verify(frame.arrayListOfId).equals(list); //verify that the ArrayList is sent to the frame and assigned to arrayListOfId in sendList method in controller
+
+        //verify(frame...) getting erorr "argument passed to verify() should be a mock but is null //
+
+    }
+
+
+    @Test
+    public void requestObjectInfo() {
+        MetService service = mock(MetService.class);
+        Call<MetFeed> call = mock(Call.class);
+        JLabel label = mock(JLabel.class);
+        JComboBox<MetFeed.DepartmentObjects.Departments> comboBox = mock(JComboBox.class);
+        MetFrame frame = mock(MetFrame.class);
+        int id = 10;
+
+        doReturn(call).when(service).getObjectInfo(id);
+
+        MetController controller = new MetController(service, frame, label, label, label, label, label, comboBox);
+
+        //when
+        controller.requestObjectInfo(id);
+
+        //then
+        verify(call).enqueue(any());
+
+    }
+
+
 
 }
